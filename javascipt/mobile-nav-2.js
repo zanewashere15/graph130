120634529211af53241932e6bf3a7e76028f0d88
zanewashere15 <zanewashere15@gmail.com>
2025-06-17 02:26:22 -0700

    32
    33

--- javascipt/mobile-nav-2.js
+++ javascipt/mobile-nav-2.js
@@ -1,6 +1,25 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('kiwi')[0]
+fetch('nav.html')
+
+ .then(response => response.text())
+
+ .then(data => {
+
+document.getElementById('nav-placeholder').innerHTML = data;
+
+// Wait until nav.html is loaded, THEN attach toggle behavior
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+
+const navbarLinks = document.getElementsByClassName('kiwi')[0];
+
+if (toggleButton && navbarLinks) {
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+
+navbarLinks.classList.toggle('active');
+
+ });
+
+ }
+
+ });
